691459ad29af23dab241d6d19440a4112a66d8f7
brunoemferreira <brunoemf@gmail.com>
2020-11-11 20:11:34 -0300

    Aula 51 - Hook useEffect

--- projeto-1/router-2/src/components/Sobre.jsx
+++ projeto-1/router-2/src/components/Sobre.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Sobre = () => {
+  
+  const [equipe, setEquipe] = useState([]);
+
+  useEffect(() => {
+    // console.log('useEffect')
+    obterDados(); 
+  },[])
+
+  const obterDados = async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await data.json();
+    // console.log(users);
+    setEquipe(users);
+;  }
+
   return (
     <div>
       <h1>Sobre</h1>
+      <ul>
+        {
+          equipe.map(item => (
+            <li key={item.id}>
+               {item.name} - {item.email}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
