3eb1068de1f7f2d4e807655a6e3e1e6dc0237f78
brunoemferreira <brunoemf@gmail.com>
2020-11-12 19:25:39 -0300

    Aula 55 - Firebase Firestore

---
+++ projeto-1/crud-firestore/src/firebase.jsx
@@ -0,0 +1,14 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDyI1hyd0LdRyqq4uMYXdorP46UjQ48yCE",
+    authDomain: "crud-udemy-react-3ee68.firebaseapp.com",
+    databaseURL: "https://crud-udemy-react-3ee68.firebaseio.com",
+    projectId: "crud-udemy-react-3ee68",
+    storageBucket: "crud-udemy-react-3ee68.appspot.com",
+    messagingSenderId: "863058122765",
+    appId: "1:863058122765:web:d0e0c47337cf6e1d9d9d44"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
